6336313dd6d00f17b3478c30fb3c166a1688f349
JeffeeH <j22132235@gmail.com>
2023-02-22 19:43:58 +0100

    update build files

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/Users/jeffeehsiung/Desktop/Esp32/espnow_public/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/Users/jeffeehsiung/esp/esp-idf/.git")
+set(GIT_DIR "/Users/jeffeehsiung/Desktop/Esp32/espnow_public/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
